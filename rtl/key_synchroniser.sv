63ead1d823202e686a677a8484aa54f856375869
j-lees <jacob.lees2527@gmail.com>
2026-05-10 18:22:22 +1000

    Update key_synchroniser.sv

--- rtl/key_synchroniser.sv
+++ rtl/key_synchroniser.sv
@@ -6,11 +6,13 @@ module key_synchroniser (
     output logic [3:0] key_sync   // active-high, synchronised
 );
     logic [3:0] key_flipped = 4'b0000;
-    initial key_sync = 4'b0000;
+    logic [3:0] key_synced = 4'b0000;
 
     always_ff @ (posedge clk) begin
         key_flipped <= ~key_n;
-        key_sync <= key_flipped;
+        key_synced <= key_flipped;
     end
+
+    assign key_sync = key_synced;
 
 endmodule
